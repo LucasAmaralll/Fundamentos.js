4e7fd2c84485e5a37931ade3b07ac0a7d4660061
Lucas do Amaral Faustino <lukinhafaustino2001@gmail.com>
2023-10-18 20:52:57 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,26 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante (números decimais)
+
+const numeroPontoFlutuante = 3.3;
+
+const pontoFlutuanteSemZero = .5; // se eu criar apenas com o ponto na frente não preciso digitar o zero
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log (novaOperacao);
+
+// NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
+
+console.log (alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O Leo disse "oi!"'; // funciona com as aspas ao contrário também
+console.log (citacao);
+
+// concatenação (+)
+
+const fraseNome = "Meu nome é ";
+const meuNome = "Lucas";
+
+console.log (fraseNome + meuNome);
+
+// template string ou template literal
+
