d8a26e4ffa3955ff250bb36d6b0d69720569731c
Lucas do Amaral Faustino <lukinhafaustino2001@gmail.com>
2023-10-25 20:50:52 -0300

    refresh

---
+++ template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Lucas";
+const idade = 2023-2001;
+const cidadeDeNascimento = "Maringá";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log (apresentacao);
+
+
